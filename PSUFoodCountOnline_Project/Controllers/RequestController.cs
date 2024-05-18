0f4e5b44f63e726b02559e1558bc59ef00294b04
Dean_sg001 <burhanurdin.sg@gmail.com>
2024-05-18 18:40:54 +0700

    create controll Request  (dev in Index)

---
+++ PSUFoodCountOnline_Project/Controllers/RequestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PSUFoodCountOnline_Project.Controllers
+{
+    public class RequestController : Controller
+    {
+        // GET: Request
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+    }
+}
