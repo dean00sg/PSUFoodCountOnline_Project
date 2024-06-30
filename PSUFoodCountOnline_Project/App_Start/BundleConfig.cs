298b130ce84362725777f46d692d1d7f80ae1866
Dean_sg001 <burhanurdin.sg@gmail.com>
2024-06-30 18:15:45 +0700

    use bundle for call css and load icons

--- PSUFoodCountOnline_Project/App_Start/BundleConfig.cs
+++ PSUFoodCountOnline_Project/App_Start/BundleConfig.cs
@@ -24,7 +24,9 @@ namespace PSUFoodCountOnline_Project
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.css",
+                      "~/Content/font-awesome.min.css")); 
         }
     }
 }
